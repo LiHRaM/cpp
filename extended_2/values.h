ab95173d221634a16357e2459f23f69b0a810f83
Hilmar Gústafsson <hilmargustafs@gmail.com>
2020-03-09 13:09:52 +0100

    Format values.h

--- extended_2/values.h
+++ extended_2/values.h
@@ -1,16 +1,14 @@
 // Daily stock price
-struct StockPrice
-{
-    unsigned day;     // day number
-    unsigned price;   // price in USD cents
+struct StockPrice {
+    unsigned day;    // day number
+    unsigned price;  // price in USD cents
 };
 
 // Temperature measurement
-class TempMeasure
-{
+class TempMeasure {
    public:
-    TempMeasure () : m{0.0} {}
-    TempMeasure (double _m) : m{_m} {}
+    TempMeasure() : m{0.0} {}
+    TempMeasure(double _m) : m{_m} {}
 
     operator double() const { return m; }
 
